528477b817695f77d6cad8bd61bf036cd7d0a05a
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-02-20 16:19:07 +0100

    Stuff

--- src/composables/Socket.io.ts
+++ src/composables/Socket.io.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 
 export default function useSocketIO() {
-  const socket = io('http://localhost:3001');
+  const socket = io('https://ultimate-tic-tac-toe-xi.herokuapp.com/');
   return {
     socket
   }
