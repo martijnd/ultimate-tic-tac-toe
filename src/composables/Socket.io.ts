7acd1c22b8d777734c2c0ed4342a9134523a719b
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-12-19 21:23:49 +0100

    Change URL

--- src/composables/Socket.io.ts
+++ src/composables/Socket.io.ts
@@ -1,9 +1,8 @@
-import { io } from 'socket.io-client'
-
+import { io } from "socket.io-client";
 
 export default function useSocketIO() {
-  const socket = io('https://ultimate-tic-tac-toe-xi.herokuapp.com/');
+  const socket = io("https://172-105-90-243.ip.linodeusercontent.com/");
   return {
-    socket
-  }
+    socket,
+  };
 }
